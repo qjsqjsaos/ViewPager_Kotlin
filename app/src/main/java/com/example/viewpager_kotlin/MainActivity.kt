cec3ef23a047cbc9fc7c82fed0405671e9de5664
sooyeolBaek <merrygoaround0726@gmail.com>
2021-04-18 19:55:19 +0900

    뷰페이저 명도랑 비율 조정 가능버전

--- app/src/main/java/com/example/viewpager_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/viewpager_kotlin/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.viewpager_kotlin
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
 
+    private val MIN_SCALE = 0.1f // 뷰가 몇퍼센트로 줄어들 것인지
+    private val MIN_ALPHA = 0.2f // 어두워지는 정도를 나타낸 듯 하다.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,10 +18,51 @@ class MainActivity : AppCompatActivity() {
 
         viewPager_idol.adapter = ViewPagerAdapter(getIdolList()) // 어댑터 생성
         viewPager_idol.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 방향을 가로로
+        viewPager_idol.setPageTransformer(ZoomOutPageTransformer()) // 애니메이션 적용
+
     }
 
     // 뷰 페이저에 들어갈 아이템
     private fun getIdolList(): ArrayList<Int> {
         return arrayListOf<Int>(R.drawable.idol1, R.drawable.idol2, R.drawable.idol3)
+    }
+
+    /* 공식문서에 있는 코드 긁어온거임 */
+    inner class ZoomOutPageTransformer : ViewPager2.PageTransformer {
+        override fun transformPage(view: View, position: Float) {
+            view.apply {
+                val pageWidth = width
+                val pageHeight = height
+                when {
+                    position < -1 -> { // [-Infinity,-1)
+                        // This page is way off-screen to the left.
+                        alpha = 0f
+                    }
+                    position <= 1 -> { // [-1,1]
+                        // Modify the default slide transition to shrink the page as well
+                        val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
+                        val vertMargin = pageHeight * (1 - scaleFactor) / 2
+                        val horzMargin = pageWidth * (1 - scaleFactor) / 2
+                        translationX = if (position < 0) {
+                            horzMargin - vertMargin / 2
+                        } else {
+                            horzMargin + vertMargin / 2
+                        }
+
+                        // Scale the page down (between MIN_SCALE and 1)
+                        scaleX = scaleFactor
+                        scaleY = scaleFactor
+
+                        // Fade the page relative to its size.
+                        alpha = (MIN_ALPHA +
+                                (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
+                    }
+                    else -> { // (1,+Infinity]
+                        // This page is way off-screen to the right.
+                        alpha = 0f
+                    }
+                }
+            }
+        }
     }
 }
